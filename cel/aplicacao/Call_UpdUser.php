b0e8106f58364eac9e9392d3ec4e166fc8d14feb
Rodrigo Santana Gonçalves <rodrigosg2000@hotmail.com>
2013-09-13 14:02:47 -0300

    Atualização

--- cel/aplicacao/Call_UpdUser.php
+++ cel/aplicacao/Call_UpdUser.php
@@ -34,102 +34,104 @@ $qrr = mysql_query($q) or die("Erro ao executar a query");
 
 ?>
 <html>
-<head>
-<title>Alterar dados de Usuário</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-</head>
-<script language="JavaScript">
-<!--
-function TestarBranco(form)
-{
-	login      = form.login.value;
-	senha      = form.senha.value;
-	senha_conf = form.senha_conf.value;
-	nome       = form.nome.value;
-	email      = form.email.value;
-
-	if (login == "")
-	{ 
-		alert ("Por favor, digite o seu Login.")
-		form.login.focus()
-      	return false;
-    }
-	if (email == "")
-	{
-		alert ( "Por favor, digite o seu e-mail.")
-      	form.email.focus();
-      	return false;
-   	}
-  	if (senha == "")
-    { 
-  	    alert ("Por favor, digite a sua senha.")
-      	form.senha.focus()
-      	return false;
-    }
-    if (nome == "")
-    { 
-        alert ("Por favor, digite o seu nome.")
-      	form.nome.focus()
-      	return false;
-    }
-   	if (senha != senha_conf)
-   	{
-      	alert ( "A senha e a confirmacao nao sao as mesmas!")
-      	form.senha.focus();
-      	return false;
-   	}
-}
-
-
-function checkEmail(email)
-{
-  	if(email.value.length > 0)
-  	{
-     	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))
-     	{
-        	return (true)
-     	}
-     	
-		alert("Atenção: o E-mail digitado não é válido.")
-		email.focus();
-		email.select();
-		return (false)
-  	}
-}
-
-
-//-->
-</SCRIPT>
-<body>
-<h3 style="text-align: center">Por favor, preencha os dados abaixo:</h3>
-<form action="updUser.php" method="post">
-  <table>
-    <tr>
-      <td>Nome:</td>
-      <td colspan="3"><input name="nome" maxlength="255" size="48" type="text" value="<?=$nome?>"></td>
-    </tr>
-    <tr>
-      <td>E-mail:</td>
-      <td colspan="3"><input name="email" maxlength="64" size="48" type="text" value="<?=$email?>" OnBlur="checkEmail(this)"></td>
-    </tr>
-    <tr>
-      <td>Login:</td>
-      <td><input name="login" maxlength="32" size="24" type="text" value="<?=$login?>"></td>
-    </tr>
-    <tr>
-      <td>Senha:</td>
-      <td><input name="senha" maxlength="32" size="16" type="password" value=""></td>
-    </tr>
-    <tr>
-      <td>Senha (confirmação):</td>
-      <td><input name="senha_conf" maxlength="32" size="16" type="password" value=""></td>
-    </tr>
-    <tr>
-      <td align="center" colspan="4" height="40" valign="bottom"><input name="submit" onClick="return TestarBranco(this.form);" type="submit" value="Atualizar"></td>
-    </tr>
-  </table>
-</form>
-<br>
-<i><a href="showSource.php?file=Call_UpdUser.php">Veja o código fonte!</a></i>
-</body>
+	<head>
+	<title>Alterar dados de Usuário</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	</head>
+	
+	<script language="JavaScript">
+		<!--
+		function TestarBranco(form)
+		{
+			login      = form.login.value;
+			senha      = form.senha.value;
+			senha_conf = form.senha_conf.value;
+			nome       = form.nome.value;
+			email      = form.email.value;
+		
+			if (login == "")
+			{ 
+				alert ("Por favor, digite o seu Login.")
+				form.login.focus()
+		      	return false;
+		    }
+			if (email == "")
+			{
+				alert ( "Por favor, digite o seu e-mail.")
+		      	form.email.focus();
+		      	return false;
+		   	}
+		  	if (senha == "")
+		    { 
+		  	    alert ("Por favor, digite a sua senha.")
+		      	form.senha.focus()
+		      	return false;
+		    }
+		    if (nome == "")
+		    { 
+		        alert ("Por favor, digite o seu nome.")
+		      	form.nome.focus()
+		      	return false;
+		    }
+		   	if (senha != senha_conf)
+		   	{
+		      	alert ( "A senha e a confirmacao nao sao as mesmas!")
+		      	form.senha.focus();
+		      	return false;
+		   	}
+		}
+		
+		
+		function checkEmail(email)
+		{
+		  	if(email.value.length > 0)
+		  	{
+		     	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))
+		     	{
+		        	return (true)
+		     	}
+		     	
+				alert("Atenção: o E-mail digitado não é válido.")
+				email.focus();
+				email.select();
+				return (false)
+		  	}
+		}
+		
+		
+		//-->
+	</SCRIPT>
+	
+	<body>
+		<h3 style="text-align: center">Por favor, preencha os dados abaixo:</h3>
+		<form action="updUser.php" method="post">
+		  <table>
+		    <tr>
+		      <td>Nome:</td>
+		      <td colspan="3"><input name="nome" maxlength="255" size="48" type="text" value="<?=$nome?>"></td>
+		    </tr>
+		    <tr>
+		      <td>E-mail:</td>
+		      <td colspan="3"><input name="email" maxlength="64" size="48" type="text" value="<?=$email?>" OnBlur="checkEmail(this)"></td>
+		    </tr>
+		    <tr>
+		      <td>Login:</td>
+		      <td><input name="login" maxlength="32" size="24" type="text" value="<?=$login?>"></td>
+		    </tr>
+		    <tr>
+		      <td>Senha:</td>
+		      <td><input name="senha" maxlength="32" size="16" type="password" value=""></td>
+		    </tr>
+		    <tr>
+		      <td>Senha (confirmação):</td>
+		      <td><input name="senha_conf" maxlength="32" size="16" type="password" value=""></td>
+		    </tr>
+		    <tr>
+		      <td align="center" colspan="4" height="40" valign="bottom"><input name="submit" onClick="return TestarBranco(this.form);" type="submit" value="Atualizar"></td>
+		    </tr>
+		  </table>
+		</form>
+		<br>
+		<i><a href="showSource.php?file=Call_UpdUser.php">Veja o código fonte!</a></i>
+	</body>
 </html>
--- cel/aplicacao/add_cenario.php
+++ cel/aplicacao/add_cenario.php
@@ -27,13 +27,13 @@ if (isset($submit))
 {
     $existenceScenario = checarCenarioExistente($_SESSION['id_projeto_corrente'],$titulo);
     ?>
-<!-- TESTS -->
-<!--
-           RET = <?=$existenceScenario?> => RET = <?PHP $existenceScenario? print("TRUE") : print("FALSE") ; ?><BR>
-       $sucesso        = <?=$sucesso?><BR>
-       _GET["sucesso"] = <?=$_GET["sucesso"]?><BR>   
-        -->
-<?PHP
+	<!-- TESTS -->
+	<!--
+		RET = <?=$existenceScenario?> => RET = <?PHP $existenceScenario? print("TRUE") : print("FALSE") ; ?><BR>
+	    $sucesso = <?=$sucesso?><BR>
+	    _GET["sucesso"] = <?=$_GET["sucesso"]?><BR>   
+	-->
+<?php
     if ($existenceScenario == true)
     {    
         print("<!-- Tentando Inserir Cenario --><BR>");
@@ -60,24 +60,25 @@ if (isset($submit))
      else
      {
      	?>
-<html>
-<head>
-<title>Projeto</title>
-</head>
-<body bgcolor="#FFFFFF">
-<p style="color: red; font-weight: bold; text-align: center">Este cenário já existe!</p>
-<br>
-<br>
-<center>
-  <a href="JavaScript:window.history.go(-1)">Voltar</a>
-</center>
-</body>
-</html>
-<?php
+		<html>
+			<head>
+				<title>Projeto</title>
+			</head>
+			<body bgcolor="#FFFFFF">
+				<p style="color: red; font-weight: bold; text-align: center">Este cenário já existe!</p>
+				<br>
+				<br>
+				<center>
+				  <a href="JavaScript:window.history.go(-1)">Voltar</a>
+				</center>
+			</body>
+		</html>
+		<?php
         return;   
      }
 	?>
-<script language="javascript1.2">
+	
+	<script language="javascript1.2">
 	
 	opener.parent.frames['code'].location.reload();
 	opener.parent.frames['text'].location.replace('main.php?id_projeto=<?=$_SESSION['id_projeto_corrente']?>');
@@ -91,137 +92,137 @@ if (isset($submit))
 <?php
 
 } 
-else 
-{	// Script chamado atraves do menu superior
-	    $nome_projeto = simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']);
-		?>
-<html>
-<head>
-<title>Adicionar Cenário</title>
-</head>
-<body>
-<script language="JavaScript">
-	<!--
-	function TestarBranco(form)
-	{
-		titulo     = form.titulo.value;
-		objetivo   = form.objetivo.value;
-		contexto   = form.contexto.value;
-		
-		if ((titulo == ""))
-		{ 
-			alert ("Por favor, digite o titulo do cenário.")
-			form.titulo.focus()
-			return false;
-		} 
-		else
-		{
-			padrao = /[\\\/\?"<>:|]/;
-			OK = padrao.exec(titulo);
-			if (OK)
+else // Script chamado atraves do menu superior 
+{
+	$nome_projeto = simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']);
+	?>
+	<html>
+		<head>
+			<title>Adicionar Cenário</title>
+		</head>
+	<body>
+		<script language="JavaScript">
+			<!--
+			function TestarBranco(form)
 			{
-				window.alert ("O título do cenário não pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
-				form.titulo.focus();
-				return false;
-			} 
+				titulo = form.titulo.value;
+				objetivo = form.objetivo.value;
+				contexto = form.contexto.value;
+				
+				if ((titulo == ""))
+				{ 
+					alert ("Por favor, digite o titulo do cenário.")
+					form.titulo.focus()
+					return false;
+				} 
+				else
+				{
+					padrao = /[\\\/\?"<>:|]/;
+					OK = padrao.exec(titulo);
+					if (OK)
+					{
+						window.alert ("O título do cenário não pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
+						form.titulo.focus();
+						return false;
+					} 
+				}
+				      
+				if ((objetivo == ""))
+				{
+					alert ("Por favor, digite o objetivo do cenário.")
+				    form.objetivo.focus()
+				    return false;
+				}    
+				      
+				if ((contexto == ""))
+				{
+					alert ("Por favor, digite o contexto do cenário.")
+				    form.contexto.focus()
+				    return false;
+				}        
+			}
+			//-->
+			
+			<?php
+			
+			// Cenário -  Incluir Cenário 
+			
+			//Objetivo:        Permitir ao usuário a inclusão de um novo cenário
+			//Contexto:        Usuário deseja incluir um novo cenário.
+			//              Pré-Condição: Login, cenário ainda não cadastrado
+			//Atores:        Usuário, Sistema
+			//Recursos:        Dados a serem cadastrados
+			//Episódios:    O sistema fornecerá para o usuário uma tela com os seguintes campos de texto:
+			//                - Nome Cenário
+			//                - Objetivo.  Restrição: Caixa de texto com pelo menos 5 linhas de escrita visíveis
+			//                - Contexto.  Restrição: Caixa de texto com pelo menos 5 linhas de escrita visíveis
+			//                - Atores.    Restrição: Caixa de texto com pelo menos 5 linhas de escrita visíveis
+			//                - Recursos.  Restrição: Caixa de texto com pelo menos 5 linhas de escrita visíveis
+			//                - Exceção.   Restrição: Caixa de texto com pelo menos 5 linhas de escrita visíveis
+			//                - Episódios. Restrição: Caixa de texto com pelo menos 16 linhas de escrita visíveis
+			//                - Botão para confirmar a inclusão do novo cenário
+			//              Restrições: Depois de clicar no botão de confirmação,
+			//                          o sistema verifica se todos os campos foram preenchidos. 
+			// Exceção:        Se todos os campos não foram preenchidos, retorna para o usuário uma mensagem avisando
+			//              que todos os campos devem ser preenchidos e um botão de voltar para a pagina anterior.
+			
+			?>
+		</SCRIPT>
+		
+		<h4>Adicionar Cenário</h4>
+		<br>
+		<?php
+		if ( $sucesso == "s" )
+		{
+		?>
+		<p style="color: blue; font-weight: bold; text-align: center">Cenário inserido com sucesso!</p>
+		<?php    
 		}
-		      
-		if ((objetivo == ""))
-		{
-			alert ("Por favor, digite o objetivo do cenário.")
-		    form.objetivo.focus()
-		    return false;
-		}    
-		      
-		if ((contexto == ""))
-		{
-			alert ("Por favor, digite o contexto do cenário.")
-		    form.contexto.focus()
-		    return false;
-		}        
-	}
-	//-->
-	
-	<?php
-	
-	// Cenário -  Incluir Cenário 
-	
-	//Objetivo:        Permitir ao usuário a inclusão de um novo cenário
-	//Contexto:        Usuário deseja incluir um novo cenário.
-	//              Pré-Condição: Login, cenário ainda não cadastrado
-	//Atores:        Usuário, Sistema
-	//Recursos:        Dados a serem cadastrados
-	//Episódios:    O sistema fornecerá para o usuário uma tela com os seguintes campos de texto:
-	//                - Nome Cenário
-	//                - Objetivo.  Restrição: Caixa de texto com pelo menos 5 linhas de escrita visíveis
-	//                - Contexto.  Restrição: Caixa de texto com pelo menos 5 linhas de escrita visíveis
-	//                - Atores.    Restrição: Caixa de texto com pelo menos 5 linhas de escrita visíveis
-	//                - Recursos.  Restrição: Caixa de texto com pelo menos 5 linhas de escrita visíveis
-	//                - Exceção.   Restrição: Caixa de texto com pelo menos 5 linhas de escrita visíveis
-	//                - Episódios. Restrição: Caixa de texto com pelo menos 16 linhas de escrita visíveis
-	//                - Botão para confirmar a inclusão do novo cenário
-	//              Restrições: Depois de clicar no botão de confirmação,
-	//                          o sistema verifica se todos os campos foram preenchidos. 
-	// Exceção:        Se todos os campos não foram preenchidos, retorna para o usuário uma mensagem avisando
-	//              que todos os campos devem ser preenchidos e um botão de voltar para a pagina anterior.
-	
-	?>
-	
-	</SCRIPT>
-<h4>Adicionar Cenário</h4>
-<br>
-<?php
-	if ( $sucesso == "s" )
-	{
-	?>
-<p style="color: blue; font-weight: bold; text-align: center">Cenário inserido com sucesso!</p>
-<?php    
-	}
-	?>
-<form action="" method="post">
-  <table>
-    <tr>
-      <td>Projeto:</td>
-      <td><input disabled size="51" type="text" value="<?=$nome_projeto?>"></td>
-    </tr>
-    
-      <td>Título:</td>
-      <td><input size="51" name="titulo" type="text" value=""></td>
-    <tr>
-      <td>Objetivo:</td>
-      <td><textarea cols="51" name="objetivo" rows="3" WRAP="SOFT"></textarea></td>
-    </tr>
-    <tr>
-      <td>Contexto:</td>
-      <td><textarea cols="51" name="contexto" rows="3" WRAP="SOFT"></textarea></td>
-    </tr>
-    <tr>
-      <td>Atores:</td>
-      <td><textarea cols="51" name="atores" rows="3" WRAP="SOFT"></textarea></td>
-    </tr>
-    <tr>
-      <td>Recursos:</td>
-      <td><textarea cols="51" name="recursos" rows="3" WRAP="SOFT"></textarea></td>
-    </tr>
-    <tr>
-      <td>Exceção:</td>
-      <td><textarea cols="51" name="excecao" rows="3" WRAP="SOFT"></textarea></td>
-    </tr>
-    <tr>
-      <td>Episódios:</td>
-      <td><textarea cols="51" name="episodios" rows="5" WRAP="SOFT"></textarea></td>
-    </tr>
-    <tr>
-      <td align="center" colspan="2" height="60"><input name="submit" type="submit" onClick="return TestarBranco(this.form);" value="Adicionar Cenário"></td>
-    </tr>
-  </table>
-</form>
-<center>
-  <a href="javascript:self.close();">Fechar</a>
-</center>
-<br>
-<i><a href="showSource.php?file=add_cenario.php">Veja o código fonte!</a></i>
-</body>
+		?>
+		<form action="" method="post">
+		  <table>
+		    <tr>
+		      <td>Projeto:</td>
+		      <td><input disabled size="51" type="text" value="<?=$nome_projeto?>"></td>
+		    </tr>
+		    
+		      <td>Título:</td>
+		      <td><input size="51" name="titulo" type="text" value=""></td>
+		    <tr>
+		      <td>Objetivo:</td>
+		      <td><textarea cols="51" name="objetivo" rows="3" WRAP="SOFT"></textarea></td>
+		    </tr>
+		    <tr>
+		      <td>Contexto:</td>
+		      <td><textarea cols="51" name="contexto" rows="3" WRAP="SOFT"></textarea></td>
+		    </tr>
+		    <tr>
+		      <td>Atores:</td>
+		      <td><textarea cols="51" name="atores" rows="3" WRAP="SOFT"></textarea></td>
+		    </tr>
+		    <tr>
+		      <td>Recursos:</td>
+		      <td><textarea cols="51" name="recursos" rows="3" WRAP="SOFT"></textarea></td>
+		    </tr>
+		    <tr>
+		      <td>Exceção:</td>
+		      <td><textarea cols="51" name="excecao" rows="3" WRAP="SOFT"></textarea></td>
+		    </tr>
+		    <tr>
+		      <td>Episódios:</td>
+		      <td><textarea cols="51" name="episodios" rows="5" WRAP="SOFT"></textarea></td>
+		    </tr>
+		    <tr>
+		      <td align="center" colspan="2" height="60"><input name="submit" type="submit" onClick="return TestarBranco(this.form);" value="Adicionar Cenário"></td>
+		    </tr>
+		  </table>
+		</form>
+		<center>
+		  <a href="javascript:self.close();">Fechar</a>
+		</center>
+		<br>
+		<i><a href="showSource.php?file=add_cenario.php">Veja o código fonte!</a></i>
+	</body>
 </html>
 <?php
 }
