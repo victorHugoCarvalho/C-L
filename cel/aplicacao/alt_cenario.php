2e9fb199bc3065ae9c29431f6a2267137e717939
Winstein Martins <sriwinstein@gmail.com>
2013-09-26 22:34:53 -0300

    corrigindo textos

--- cel/aplicacao/alt_cenario.php
+++ cel/aplicacao/alt_cenario.php
@@ -38,7 +38,7 @@ if (isset($submit))
 		opener.parent.frames['text'].location.replace('main.php?id_projeto=<?=$_SESSION['id_projeto_corrente']?>');
 	
 	</script>
-<h4>Opera��o efetuada com sucesso!</h4>
+<h4>Opera&ccedil;&atilde;o efetuada com sucesso!</h4>
 <script language="javascript1.3">
 	
 		self.close();
@@ -49,7 +49,7 @@ if (isset($submit))
 else  // Script chamado atraves do link no cenario corrente
 {
 
-    $nome_projeto = simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']);
+    $project_name = simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']);
 
     $query = "SELECT * FROM cenario WHERE id_cenario = $id_cenario";
     $executeQuery = mysql_query($query) or die("Erro ao executar a query");
@@ -73,20 +73,20 @@ else  // Script chamado atraves do link no cenario corrente
 	?>
 <html>
 <head>
-<title>Alterar Cen�rio</title>
+<title>Alterar Cen&aacute;rio</title>
 </head>
 <body>
-<h4>Alterar Cen�rio</h4>
+<h4>Alterar Cen&aacute;rio</h4>
 <br>
 <form action="?id_projeto=<?=$id_projeto?>" method="post">
   <table>
     <tr>
       <td>Projeto:</td>
-      <td><input disabled size="48" type="text" value="<?=$nome_projeto?>"></td>
+      <td><input disabled size="48" type="text" value="<?=$project_name?>"></td>
     </tr>
     <input type="hidden" name="id_cenario" value="<?=$result['id_cenario']?>">
     
-      <td>T�tulo:</td>
+      <td>T&iacute;tulo:</td>
       <?php $result['titulo'] = preg_replace("'<[\/\!]*?[^<>]*?>'si", "", $result['titulo']); ?>
       <input type="hidden" name="title" value="<?=$result['titulo']?>">
       <td><input disabled maxlength="128" name="titulo2" size="48" type="text" value="<?=$result['titulo']?>"></td>
@@ -115,13 +115,13 @@ else  // Script chamado atraves do link no cenario corrente
 </textarea></td>
     </tr>
     <tr>
-      <td>Exce��o:</td>
+      <td>Exce&ccedil;&atilde;o:</td>
       <? $result['excecao'] = preg_replace("'<[\/\!]*?[^<>]*?>'si", "", $result['excecao']); ?>
       <td><textarea name="exception" cols="48" rows="3"><?=$result['excecao']?>
 </textarea></td>
     </tr>
     <tr>
-      <td>Epis�dios:</td>
+      <td>Epis&oacute;dios:</td>
       <? $result['episodios'] = preg_replace("'<[\/\!]*?[^<>]*?>'si", "", $result['episodios']); ?>
       <td><textarea  cols="48" name="episodes" rows="5"><?=$result['episodios']?>
 </textarea></td>
@@ -131,10 +131,10 @@ else  // Script chamado atraves do link no cenario corrente
       <td><textarea name="justification" cols="48" rows="2"></textarea></td>
     </tr>
     <tr>
-      <td colspan="2"><b><small>Essa justificativa � necess�ria apenas para aqueles usu�rios que n�o s�o administradores.</small></b></td>
+      <td colspan="2"><b><small>Essa justificativa &eacute; necess&aacute;ria apenas para aqueles usu&aacute;rios que n&atilde;o s&atilde;o administradores.</small></b></td>
     </tr>
     <tr>
-      <td align="center" colspan="2" height="60"><input name="submit" type="submit" value="Alterar Cen�rio" onClick="updateOpener()"></td>
+      <td align="center" colspan="2" height="60"><input name="submit" type="submit" value="Alterar Cen&aacute;rio" onClick="updateOpener()"></td>
     </tr>
   </table>
 </form>
@@ -142,7 +142,7 @@ else  // Script chamado atraves do link no cenario corrente
   <a href="javascript:self.close();">Fechar</a>
 </center>
 <br>
-<i><a href="showSource.php?file=alt_cenario.php">Veja o c�digo fonte!</a></i>
+<i><a href="showSource.php?file=alt_cenario.php">Veja o c&oacute;digo fonte!</a></i>
 </body>
 </html>
 <?php
