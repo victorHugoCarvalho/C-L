56cf9bfc6965a6c5b552819dfc17337382baa449
Winstein Martins <sriwinstein@gmail.com>
2013-09-30 20:49:31 -0300

    iniciando tradução de add_usuario

--- cel/aplicacao/add_usuario.php
+++ cel/aplicacao/add_usuario.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+session_start();
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -9,16 +9,16 @@
 include("funcoes_genericas.php");
 include_once("bd.inc");
 
-$primeira_vez = "true";
+$firstTime = "true";
 
 include("httprequest.inc");
 
-if (isset($submit)) // Se chamado pelo botao de submit
+if (isset($submit)) // Called by the submit button
 {
-    $primeira_vez = "false";
-    // ** Cenario "Inclusao de Usuario Independente" **
-    // O sistema checa se todos os campos estao preenchidos. Se algum nao estiver, o
-    // sistema avisa pro usuario que todos os campos devem ser preenchidos.
+    $firstTime = "false";
+    // ** Scenario "Adding independet user" **
+    // The system checks if all the fields are filled. If any field is not filled, 
+    //   the system warns the user that all the field should be filled.  
     if ($nome == "" || $email == "" || $login == "" || $senha == "" || $senha_conf == "")
 	{
         $p_style = "color: red; font-weight: bold";
@@ -27,7 +27,7 @@ if (isset($submit)) // Se chamado pelo botao de submit
     }
     else
     {
-        // Testa se as senhas fornecidas pelo usuario sao iguais.
+    	// Check if the passwords typed by the user are equal
         if ($senha != $senha_conf)
         {
             $p_style = "color: red; font-weight: bold";
@@ -66,39 +66,34 @@ if (isset($submit)) // Se chamado pelo botao de submit
             $SendQuery = mysql_query($query) or die("Erro ao enviar a query");
             if (mysql_num_rows($SendQuery)) // Se ja existe algum usuario com este login
             {        
-				// $p_style = "color: red; font-weight: bold";
-				// $p_text = "Login ja existente no sistema. Favor escolher outro login.";
-				// recarrega("?p_style=$p_style&p_text=$p_text&nome=$nome&email=$email&senha=$senha&senha_conf=$senha_conf&novo=$novo");
-	
-	
-				// Cen�rio - Adicionar Usuario
+				// Scenaro - Add User
 				
-				// Objetivo:  Permitir ao Administrador criar novos usuarios.
-				// Contexto:  O Administrador deseja adicionar novos usuarios (nao cadastrados)
-				//            criando novos  usuarios ao projeto selecionado.
-				//Pre-Condicoes: Login
-				// Atores:    Administrador
-				// Recursos:  Dados do usu�rio
-				// Episodios: O Administrador clica no link Adicionar usuario (nao existente) neste projeto,
-				//            entrando com as informa�oes do novo usuario: nome, email, login e senha.
-				//            Caso o login ja exista, aparecer uma mensagem de erro na tela informando que
-				//            este login ja existe.
+				// Objective:  Allow the manager to create new users
+				// Context:  The manager wants to add new users (not registered),
+				//     creating new users to the selected project
+				// Pre-Conditions:  Login
+				// Actors:  Manager
+				// Resources:  Users data
+				// Episodes:  The manager clicks on the link Add User (not registered) on this project,
+				//     typing the new users information: name, email, login and password.
+				//     If the login already exists, show an error message on the screen
+				//       informing that this login already exists
 
                 ?>
-<script language="JavaScript">
+				<script language="JavaScript">
                     alert ("Login j&aacute; existente no sistema. Favor escolher outro login.")
                 </script>
-<?php
+				<?php
                 recarrega("?novo=$novo");
             }
-            else // Cadastro passou por todos os testes -- ja pode ser incluido na BD
+            else // Passed through all the test. Can be added to the DB
 			{
-				/* Substitui todas as ocorrencias de ">" e "<" por " " */
+				/* Replace all occurences of ">" and "<" for " " */
 				$nome  = str_replace( ">" , " " , str_replace ( "<" , " " , $nome  ) ) ;
 				$login = str_replace( ">" , " " , str_replace ( "<" , " " , $login ) ) ;
 				$email = str_replace( ">" , " " , str_replace ( "<" , " " , $email ) ) ;
 				
-				// Criptografando a senha
+				// Encrypting the password
 				$senha = md5($senha);
                 $query = "INSERT INTO usuario (nome, login, email, senha) VALUES ('$nome', '$login', '$email', '$senha')";
                 mysql_query($query) or die("Erro ao cadastrar o usuario");
@@ -108,35 +103,35 @@ if (isset($submit)) // Se chamado pelo botao de submit
     }   // else
 } else if (isset($cadastrado))
 {
-    // Cadastro concluido. Dependendo de onde o usuario veio,
+    // Registration completed. Dependendo de onde o usuario veio,
     // devemos manda-lo para um lugar diferente.
 
-    if ($novo == "true") // Veio da tela inicial de login
+    if ($novo == "true") // If the user came from the login screen
     {
 
-        // ** Cenario "Inclusao de Usuario Independente" **
-        // O usuario acabou de cadastrar-se no sistema, devemos
-        // redireciona-lo para a parte de inclusao de projetos
+        // ** Scenario "Adding a new independent user" **
+        // The user just registered in the system, we should redirect him 
+        // to the adding projects part
 
-        // Registra que o usuario esta logado com o login recem-cadastrado
+        // Register that the user is logged with the newly registered login
 		
-		// Cen�rio - Incluir usu�rio independente 
+		// Scenario - Add independent user
 		
-		// Objetivo:  Permitir um usu�rio, que n�o esteja cadastrado como administrador, se cadastrar 
-		//            com o perfil de administrador	
-		// Contexto:  Sistema aberto Usu�rio deseja cadastrar-se ao sistema como administrador. 
-		//            Usu�rio na tela de cadastro de usu�rio 
-		//            Pr�-Condi��o: Usu�rio ter acessado ao sistema	
-		// Atores:    Usu�rio, Sistema	
-		// Recursos:  Interface, Banco de Dados	
-		// Epis�dios:  Caso aquele login digitado n�o exista, o sistema cadastra esse usu�rio 
-		//               como administrador no banco de dados,  possibilitando:
-		//              - Redirecion�-lo  para a interface de CADASTRAR NOVO PROJETO; 
+		// Objective:  Allow a not registered user, to register himself with the manager
+		//     profile
+		// Context:  Openen system. User wants to register himself in the system as a manager
+		//     User on the user registration screen       
+		// Pre-Conditions:  User must have accessed the system
+		// Actors:  User, System	
+		// Resources:  Interface, Database
+		// Episodes:  If the typed login does not exist, the system register this user
+		//     in the database as a manager, enabling:
+		//       - Redirect him to the ADD NEW PROJECT screen
 
         $id_usuario_corrente = simple_query("id_usuario", "usuario", "login = '$login'");
         $_SESSION['id_usuario_corrente'] = $id_usuario_corrente;
-?>
-<script language="javascript1.3">
+		?>
+		<script language="javascript1.3">
 
 			// Redireciona o usuario para a parte de inclusao de projetos
 			opener.location.replace('index.php');
@@ -144,16 +139,15 @@ if (isset($submit)) // Se chamado pelo botao de submit
 			self.close();
 
 		</script>
-<?php
+		<?php
     }
     else
 	{
-	    // ** Cenario "Edicao de Usuario" **
-	    // O administrador do projeto acabou de incluir o usuario.
-	    // Devemos agora adicionar o usuario incluido no projeto
-	    // do administrador.
+	    // ** Scenario "Editing user" **
+	    // The project manager just added the user
+	    // Now we should add the added user to the manager project 
 	
-	    // Conexao com a base de dados
+	    // Database connection
 	    $connected_SGBD = bd_connect() or die("Erro ao conectar ao SGBD");
 	    // $login eh o login do usuario incluido, passado na URL
 	    $id_usuario_incluido = simple_query("id_usuario", "usuario", "login = '$login'");
@@ -163,25 +157,29 @@ if (isset($submit)) // Se chamado pelo botao de submit
 	
 	    $nome_usuario = simple_query("nome", "usuario", "id_usuario = $id_usuario_incluido");
 	    $nome_projeto = simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']);
-?>
-<script language="javascript1.3">
+		?>
+		<script language="javascript1.3">
 		
 			document.writeln('<p style="color: blue; font-weight: bold; text-align: center">Usu&aacute;rio <b><?=$nome_usuario?></b> cadastrado e inclu&iacute;do no projeto <b><?=$nome_projeto?></b></p>');
 			document.writeln('<p align="center"><a href="javascript:self.close();">Fechar</a></p>');
 		
 		</script>
-<?php
+		<?php
     }
 }
-else // Script chamado normalmente
+else // Script called normally
 {    
     if (empty($p_style))
 	{
         $p_style = "color: green; font-weight: bold";
         $p_text = "Favor preencher os dados abaixo:";
     }
+    else
+    {
+		// nothing to do    	
+    }
 
-    if ($primeira_vez)
+    if ($firstTime)
     {
          $email ="";
 	     $login ="";
@@ -190,93 +188,108 @@ else // Script chamado normalmente
          $senha_conf = "";
 
     }
-?>
-<html>
-<head>
-<title>Cadastro de Usu&aacute;rio</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-</head>
-<body>
-<script language="JavaScript">
-	    	<!--
-	        function verifyEmail(form)
-	        {
-	            email = form.email.value;
-	            //verifica se o email contem um @
-	            i = email.indexOf("@");
-	            if (i == -1)
-	        	{
-	        	    alert('Aten��o: o E-mail digitado n�o � v�lido.');
-	        	    return false;
-	        	}
-	        }
-			
+    else
+    {
+    	// nothing to do
+    }
+	?>
+	<html>
+		<head>
+			<title>Cadastro de Usu&aacute;rio</title>
+			<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		</head>
+		<body>
+		<script language="JavaScript">
+			<!--
+		    function verifyEmail(form)
+		    {
+		    	email = form.email.value;
+		        // checking if the email contais a @
+		        i = email.indexOf("@");
+		        if (i == -1)
+		        {
+		        	alert('Aten\u00e7\u00e3o: o E-mail digitado n\u00e3o \u00e9 v\u00e1lido.');
+		        	return false;
+		        }
+		        else
+		        {
+		        	// nothing to do
+		        }
+			}
+				
 			function checkEmail(email)
 			{
 				if(email.value.length > 0)
 				{
 					if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))
-				    {
+					{
 						return (true)
-				    }
-				    
-				alert("Aten��o: o E-mail digitado n�o � v�lido.")
-				email.focus();
-				email.select();
-				return (false)
+					}
+					else
+					{
+						// nothing to do
+					}
+					    
+					alert("Aten\u00e7\u00e3o: o E-mail digitado n\u00e3o \u00e9 v\u00e1lido.")
+					email.focus();
+					email.select();
+					return (false)
+				}
+				else
+				{
+					// nothing to do
 				}
 			}
+		    //-->
 	
-	    	//-->
-
-    	</SCRIPT>
-<p style="<?=$p_style?>">
-  <?=$p_text?>
-</p>
-<form action="?novo=<?=$novo?>" method="post">
-  <table>
-    <tr>
-      <td>Nome:</td>
-      <td colspan="3"><input name="nome" maxlength="255" size="48" type="text" value="<?=$nome?>"></td>
-    </tr>
-    <tr>
-      <td>E-mail:</td>
-      <td colspan="3"><input name="email" maxlength="64" size="48" type="text" value="<?=$email?>" OnBlur="checkEmail(this)"></td>
-    </tr>
-    <tr>
-      <td>Login:</td>
-      <td><input name="login" maxlength="32" size="24" type="text" value="<?=$login?>"></td>
-    </tr>
-    <tr>
-      <td>Senha:</td>
-      <td><input name="senha" maxlength="32" size="16" type="password" value="<?=$senha?>"></td>
-      <td>Senha (confirma&ccedil;&atilde;o):</td>
-      <td><input name="senha_conf" maxlength="32" size="16" type="password" value=""></td>
-    </tr>
-    <tr>
-      <?php
-
-// Cenario - Adicionar Usuario
-
-// Objetivo:  Permitir ao Administrador criar novos usuarios.
-// Contexto:  O Administrador deseja adicionar novos usuarios (nao cadastrados) criando novos
-//              usuarios ao projeto selecionado.
-// Pre-Condicoes: Login
-// Atores:    Administrador
-// Recursos:  Dados do usuario
-// Episodios: Clicando no botao Cadastrar para confirmar adicionando novo
-//             usuario ao projeto selecionado.
-//            O novo usuario criado recebera uma mensagem via email com seu login e senha.
-
-?>
-      <td align="center" colspan="4" height="40" valign="bottom"><input name="submit" onClick="return verifyEmail(this.form);" type="submit" value="Cadastrar"></td>
-    </tr>
-  </table>
-</form>
-<br>
-<i><a href="showSource.php?file=add_usuario.php">Veja o c&oacute;digo fonte!</a></i>
-</body>
-</html>
+		</SCRIPT>
+		<p style="<?=$p_style?>">
+			<?=$p_text?>
+		</p>
+		<form action="?novo=<?=$novo?>" method="post">
+			<table>
+	    		<tr>
+	      			<td>Nome:</td>
+	      			<td colspan="3"><input name="nome" maxlength="255" size="48" type="text" value="<?=$nome?>"></td>
+	    		</tr>
+	    		<tr>
+	      			<td>E-mail:</td>
+	      			<td colspan="3"><input name="email" maxlength="64" size="48" type="text" value="<?=$email?>" OnBlur="checkEmail(this)"></td>
+	    		</tr>
+	    		<tr>
+	      			<td>Login:</td>
+	      			<td><input name="login" maxlength="32" size="24" type="text" value="<?=$login?>"></td>
+	    		</tr>
+	    		<tr>
+	      			<td>Senha:</td>
+	      			<td><input name="senha" maxlength="32" size="16" type="password" value="<?=$senha?>"></td>
+	      			<td>Senha (confirma&ccedil;&atilde;o):</td>
+	      			<td><input name="senha_conf" maxlength="32" size="16" type="password" value=""></td>
+	    		</tr>
+	    		<tr>
+		      		<?php
+		
+					// Scenario - Add user
+					
+					// Objective:  Allows the manager to create new users
+					// Context:  The manager wants to add new users (not registered) creating news
+					//  user to the selected project
+					// Pre-Conditions:  Login
+					// Actors:  Manager
+					// Resources:  Users data
+					// Episodes:  Clicking on the registration button to confirm the addition
+					//   of the new user to the selected project
+					//			  The new created user will receive via email his login and password
+		
+					?>
+	    			<td align="center" colspan="4" height="40" valign="bottom"><input name="submit" onClick="return verifyEmail(this.form);" type="submit" value="Cadastrar"></td>
+	    		</tr>
+	  		</table>
+		</form>
+		<br>
+		<i><a href="showSource.php?file=add_usuario.php">Veja o c&oacute;digo fonte!</a></i>
+		</body>
+	</html>
 <?php
 }
 ?>
