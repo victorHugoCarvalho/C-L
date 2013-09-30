1529716a4a5f48be3af657d934519138bab2bf9c
Rodrigo Santana Gonçalves <rodrigosg2000@hotmail.com>
2013-09-30 18:51:36 -0300

    Aplicação de técnicas de alto e baixo nível

--- cel/aplicacao/add_cenario.php
+++ cel/aplicacao/add_cenario.php
@@ -59,26 +59,25 @@ if (isset($submit))
      else
      {
      	?>
-		<html>
-			<head>
-				<title>Projeto</title>
-			</head>
-			<body bgcolor="#FFFFFF">
-				<p style="color: red; font-weight: bold; text-align: center">Este cen&aacute;rio j&aacute; existe!</p>
-				<br>
-				<br>
-				<center>
-				  <a href="JavaScript:window.history.go(-1)">Voltar</a>
-				</center>
-			</body>
-		</html>
-		<?php
+            <html>
+                <head>
+                        <title>Projeto</title>
+                </head>
+                <body bgcolor="#FFFFFF">
+                        <p style="color: red; font-weight: bold; text-align: center">Este cen&aacute;rio j&aacute; existe!</p>
+                        <br>
+                        <br>
+                        <center>
+                            <a href="JavaScript:window.history.go(-1)">Voltar</a>
+                        </center>
+                </body>
+            </html>
+        <?php
         return;   
      }
 	?>
 	
 	<script language="javascript1.2">
-	
 		opener.parent.frames['code'].location.reload();
 		opener.parent.frames['text'].location.replace('main.php?idProject=<?=$_SESSION['id_projeto_corrente']?>');
 		//self.close();
@@ -90,149 +89,149 @@ if (isset($submit))
 } 
 else // Script called via the top menu
 {
-	$nameProject = simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']);
+	$projectName = simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']);
 	?>
 	<html>
-		<head>
-			<title>Adicionar Cen&aacute;rio</title>
-		</head>
-	<body>
-		<script language="JavaScript">
-			<!--
-			function blankTest(form)
-			{
-				title = form.title.value;
-				objective = form.objective.value;
-				context = form.context.value;
-				
-				if ((title == ""))
-				{ 
-					alert ("Por favor, digite o titulo do cen\u00e1rio.")
-					form.title.focus()
-					return false;
-				} 
-				else
-				{
-					pattern = /[\\\/\?"<>:|]/;
-					OK = pattern.exec(title);
-					if (OK)
-					{
-						window.alert ("O t\u00edtulo do cen\u00e1rio n\u00e3o pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
-						form.title.focus();
-						return false;
-					} 
-					else
-					{
-					    //Nothing to do.
-					}
-				}
-				      
-				if ((objective == ""))
-				{
-					alert ("Por favor, digite o objetivo do cen\u00e1rio.")
-				    form.objective.focus()
-				    return false;
-				}
-				else
-				{
-				    //Nothing to do.
-				}    
-				      
-				if ((context == ""))
-				{
-					alert ("Por favor, digite o contexto do cen\u00e1rio.")
-				    form.context.focus()
-				    return false;
-				}
-				else
-				{
-				    //Nothing to do.
-				}
-				
-				return true;
-			}
-			//-->
-			
-			<?php
-			
-			// Scenario - insert scenario 
-			
-			// Objective: Allow the user to insert a new scenario
-			// Context: User wants to insert a new scenario
-			// Pre-Conditions: Login, not registered scenario
-			// Actors: User, System
-			// Resources: Data to be registered
-			// Episodes: The system will provide the following fields on the users screen
-			//   - Scenario name.
-			//   - Objective.  Restrictions: Text box with at least 5 visible writing lines.
-			//   - Context.  Restrictions: Text box with at least 5 visible writing lines.
-			//   - Actors.  Restrictions: Text box with at least 5 visible writing lines.
-			//   - Resources.  Restrictions: Text box with at least 5 visible writing lines.
-			//   - Exception.  Restrictions: Text box with at least 5 visible writing lines.
-			//   - Episodes.  Restrictions: Text box with at least 16 visible writing lines.
-			//   - Button to confirm the inclusion of a new scenario.
-			//     Restrictions: After clicking on the confirmation button,
-			//       the system verifies if all fields are filled.
-			// Exception: Se todos os campos n�o foram preenchidos, retorna para o usu�rio uma mensagem avisando
-			//   que todos os campos devem ser preenchidos e um bot�o de voltar para a pagina anterior.
-			
-			?>
-		</SCRIPT>
-		
-		<h4>Adicionar Cen&aacute;rio</h4>
-		<br>
-		<?php
-		if ($success == YES)
-		{
-			?>
-			<p style="color: blue; font-weight: bold; text-align: center">Cen&aacute;rio inserido com sucesso!</p>
-			<?php    
-		}
-		?>
-		<form action="" method="post">
-		  <table>
-		    <tr>
-		      <td>Projeto:</td>
-		      <td><input disabled size="51" type="text" value="<?=$nameProject?>"></td>
-		    </tr>
-		      <td>T&iacute;tulo:</td>
-		      <td><input size="51" name="title" type="text" value=""></td>
-		    <tr>
-		      <td>Objetivo:</td>
-		      <td><textarea cols="51" name="objective" rows="3" WRAP="SOFT"></textarea></td>
-		    </tr>
-		    <tr>
-		      <td>Contexto:</td>
-		      <td><textarea cols="51" name="context" rows="3" WRAP="SOFT"></textarea></td>
-		    </tr>
-		    <tr>
-		      <td>Atores:</td>
-		      <td><textarea cols="51" name="actors" rows="3" WRAP="SOFT"></textarea></td>
-		    </tr>
-		    <tr>
-		      <td>Recursos:</td>
-		      <td><textarea cols="51" name="resources" rows="3" WRAP="SOFT"></textarea></td>
-		    </tr>
-		    <tr>
-		      <td>Exce&ccedil;&atilde;o:</td>
-		      <td><textarea cols="51" name="exception" rows="3" WRAP="SOFT"></textarea></td>
-		    </tr>
-		    <tr>
-		      <td>Epis&oacute;dios:</td>
-		      <td><textarea cols="51" name="episodes" rows="5" WRAP="SOFT"></textarea></td>
-		    </tr>
-		    <tr>
-		      <td align="center" colspan="2" height="60"><input name="submit" type="submit" onClick="return blankTest(this.form);" value="Adicionar Cen&aacute;rio"></td>
-		    </tr>
-		  </table>
-		</form>
-		<center>
-		  <a href="javascript:self.close();">Fechar</a>
-		</center>
-		<br>
-		<i><a href="showSource.php?file=add_cenario.php">Veja o c&oacute;digo fonte!</a></i>
-	</body>
-</html>
-<?php
+            <head>
+                    <title>Adicionar Cen&aacute;rio</title>
+            </head>
+            <body>
+                <script language="JavaScript">
+                        <!--
+                        function blankTest(form)
+                        {
+                                title = form.title.value;
+                                objective = form.objective.value;
+                                context = form.context.value;
+
+                                if ((title == ""))
+                                { 
+                                        alert ("Por favor, digite o titulo do cen\u00e1rio.")
+                                        form.title.focus()
+                                        return false;
+                                } 
+                                else
+                                {
+                                        pattern = /[\\\/\?"<>:|]/;
+                                        OK = pattern.exec(title);
+                                        if (OK)
+                                        {
+                                                window.alert ("O t\u00edtulo do cen\u00e1rio n\u00e3o pode conter nenhum dos seguintes caracteres:   / \\ : ? \" < > |");
+                                                form.title.focus();
+                                                return false;
+                                        } 
+                                        else
+                                        {
+                                            //Nothing to do.
+                                        }
+                                }
+
+                                if ((objective == ""))
+                                {
+                                        alert ("Por favor, digite o objetivo do cen\u00e1rio.")
+                                    form.objective.focus()
+                                    return false;
+                                }
+                                else
+                                {
+                                    //Nothing to do.
+                                }    
+
+                                if ((context == ""))
+                                {
+                                        alert ("Por favor, digite o contexto do cen\u00e1rio.")
+                                    form.context.focus()
+                                    return false;
+                                }
+                                else
+                                {
+                                    //Nothing to do.
+                                }
+
+                                return true;
+                        }
+                        //-->
+
+                        <?php
+
+                        // Scenario - insert scenario 
+
+                        // Objective: Allow the user to insert a new scenario
+                        // Context: User wants to insert a new scenario
+                        // Pre-Conditions: Login, not registered scenario
+                        // Actors: User, System
+                        // Resources: Data to be registered
+                        // Episodes: The system will provide the following fields on the users screen
+                        //   - Scenario name.
+                        //   - Objective.  Restrictions: Text box with at least 5 visible writing lines.
+                        //   - Context.  Restrictions: Text box with at least 5 visible writing lines.
+                        //   - Actors.  Restrictions: Text box with at least 5 visible writing lines.
+                        //   - Resources.  Restrictions: Text box with at least 5 visible writing lines.
+                        //   - Exception.  Restrictions: Text box with at least 5 visible writing lines.
+                        //   - Episodes.  Restrictions: Text box with at least 16 visible writing lines.
+                        //   - Button to confirm the inclusion of a new scenario.
+                        //     Restrictions: After clicking on the confirmation button,
+                        //       the system verifies if all fields are filled.
+                        // Exception: Se todos os campos n�o foram preenchidos, retorna para o usu�rio uma mensagem avisando
+                        //   que todos os campos devem ser preenchidos e um bot�o de voltar para a pagina anterior.
+
+                        ?>
+                </SCRIPT>
+
+                <h4>Adicionar Cen&aacute;rio</h4>
+                <br>
+                <?php
+                if ($success == YES)
+                {
+                        ?>
+                        <p style="color: blue; font-weight: bold; text-align: center">Cen&aacute;rio inserido com sucesso!</p>
+                        <?php    
+                }
+                ?>
+                <form action="" method="post">
+                  <table>
+                    <tr>
+                      <td>Projeto:</td>
+                      <td><input disabled size="51" type="text" value="<?=$projectName?>"></td>
+                    </tr>
+                      <td>T&iacute;tulo:</td>
+                      <td><input size="51" name="title" type="text" value=""></td>
+                    <tr>
+                      <td>Objetivo:</td>
+                      <td><textarea cols="51" name="objective" rows="3" WRAP="SOFT"></textarea></td>
+                    </tr>
+                    <tr>
+                      <td>Contexto:</td>
+                      <td><textarea cols="51" name="context" rows="3" WRAP="SOFT"></textarea></td>
+                    </tr>
+                    <tr>
+                      <td>Atores:</td>
+                      <td><textarea cols="51" name="actors" rows="3" WRAP="SOFT"></textarea></td>
+                    </tr>
+                    <tr>
+                      <td>Recursos:</td>
+                      <td><textarea cols="51" name="resources" rows="3" WRAP="SOFT"></textarea></td>
+                    </tr>
+                    <tr>
+                      <td>Exce&ccedil;&atilde;o:</td>
+                      <td><textarea cols="51" name="exception" rows="3" WRAP="SOFT"></textarea></td>
+                    </tr>
+                    <tr>
+                      <td>Epis&oacute;dios:</td>
+                      <td><textarea cols="51" name="episodes" rows="5" WRAP="SOFT"></textarea></td>
+                    </tr>
+                    <tr>
+                      <td align="center" colspan="2" height="60"><input name="submit" type="submit" onClick="return blankTest(this.form);" value="Adicionar Cen&aacute;rio"></td>
+                    </tr>
+                  </table>
+                </form>
+                <center>
+                  <a href="javascript:self.close();">Fechar</a>
+                </center>
+                <br>
+                <i><a href="showSource.php?file=add_cenario.php">Veja o c&oacute;digo fonte!</a></i>
+            </body>
+        </html>
+        <?php
 }
 ?>
