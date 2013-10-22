9085107317f152decbc58dc20116aa556198d76d
victor hugo alves de carvalho <victorhugoac.df@gmail.com>
2013-10-22 15:18:47 -0200

    Criacao do teste unitario testIsAdminReturnTrue na classe DataBaselsAdminTest

--- cel/test/DataBaseIsAdminTest.php
+++ cel/test/DataBaseIsAdminTest.php
@@ -5,7 +5,7 @@ include ("../aplicacao/dataBase/DatabaseProject.php");
 
 class DataBaseIsAdminTest extends PHPUnit_Framework_TestCase
 {
-    public function testIsAdmin()
+    public function testIsAdminReturnFalse()
     {
         $_SESSION['id_usuario_corrente'] = "Teste";
         $id_projeto = includeProject("Nome Teste", "Descrição teste");
@@ -16,6 +16,26 @@ class DataBaseIsAdminTest extends PHPUnit_Framework_TestCase
         removeProject($id_projeto);
     }
     
+    public function testIsAdminReturnTrue()
+    {
+        $_SESSION['id_usuario_corrente'] = "Teste";
+        $id_projeto = includeProject("Nome Teste", "Descrição teste");
+        $id_usuario = 10;
+        $gerente = 1;
+        
+        $queryResult = "INSERT INTO participa (id_usuario, id_projeto,gerente) 
+            VALUES ($id_projeto, $id_usuario,$gerente)";
+        
+        mysql_query($queryResult);
+        
+        $retorno = is_admin($id_usuario, $id_projeto);
+        $this->assertTrue($retorno);
+        
+        $queryResultDelete = "DELETE FROM participa 
+        WHERE gerente = $gerente AND id_usuario = $id_projeto AND id_projeto = $id_projeto";
+        mysql_query($queryResultDelete);
+        removeProject($id_projeto);
+    }
 }
 
 ?>
