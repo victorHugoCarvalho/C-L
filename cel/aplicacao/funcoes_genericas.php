491e9e6ec174b761058c01a64f00f41b8d0d3ef1
Winstein Martins <sriwinstein@gmail.com>
2013-11-17 19:42:43 -0200

    assertiva para a função tratarPedidoCenario

--- cel/aplicacao/funcoes_genericas.php
+++ cel/aplicacao/funcoes_genericas.php
@@ -1824,6 +1824,8 @@ if (!(function_exists("tratarPedidoCenario")))
 {
     function tratarPedidoCenario($id_pedido)
     {
+    	assert($id_pedido != null, "id_pedido must not be null");
+    	
         $DB = new PGDB () ;
         $select_pedido_cenario = new QUERY ($DB) ;
         $delete = new QUERY ($DB) ;
