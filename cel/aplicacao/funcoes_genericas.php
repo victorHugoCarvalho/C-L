d9728e7cb19443a86faf7259ab2f51a5aa5dfd4a
Winstein Martins <sriwinstein@gmail.com>
2013-11-17 19:44:02 -0200

    assertiva para a função tratarPedidoConceito

--- cel/aplicacao/funcoes_genericas.php
+++ cel/aplicacao/funcoes_genericas.php
@@ -1973,6 +1973,8 @@ if (!(function_exists("tratarPedidoConceito")))
 {
     function tratarPedidoConceito($id_pedido)
     {
+    	assert($id_pedido != null, "id_pedido must not be null");
+    	
         $DB = new PGDB();
         $select_pedido_conceito = new QUERY($DB);
         $delete = new QUERY($DB);
