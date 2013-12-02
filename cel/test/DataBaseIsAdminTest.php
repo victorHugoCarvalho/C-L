d693cf3dc899b5edb5815426206f2eaba3c44e49
Rodrigo Gonçalves <rodrigosg2000@hotmail.com>
2013-12-01 23:28:59 -0200

    Teste para função check_project_permission retornando true

--- cel/test/DataBaseIsAdminTest.php
+++ cel/test/DataBaseIsAdminTest.php
@@ -30,17 +30,15 @@ class DataBaseIsAdminTest extends PHPUnit_Framework_TestCase
         
         $check = is_admin($id_user, $id_newProject);
         
-                $queryResultDelete = "DELETE FROM participa 
+        $queryResultDelete = "DELETE FROM participa 
         WHERE gerente = $admin AND id_projeto = $id_newProject";
         mysql_query($queryResultDelete);
         removeProject($id_newProject);
         
         $this->assertTrue($check);
-        
-
     }
     
-    public function testProjectPermissionReturnTrue()
+    public function testProjectPermissionReturnFalse()
     {
         $_SESSION['id_usuario_corrente'] = "Teste";
         $id_user = 10;
@@ -49,6 +47,26 @@ class DataBaseIsAdminTest extends PHPUnit_Framework_TestCase
         $check = check_project_permission($id_user, $id_newProject);
         $this->assertEquals(false, $check);
         removeProject($id_newProject);
+    }
+    
+    public function testProjectPermissionReturnTrue()
+    {
+        $_SESSION['id_usuario_corrente'] = "Teste";
+        $id_newProject = includeProject("Nome Teste", "Descrição teste");
+        $id_user = 10;
+        $admin = 1;
+        
+        $query = "INSERT INTO participa (id_usuario, id_projeto) 
+                  VALUES ($id_user, $id_newProject)";
+        
+        mysql_query($query);
+        $check = check_project_permission($id_user, $id_newProject);
+        
+        $queryResultDelete = "DELETE FROM participa id_projeto = $id_newProject";
+        mysql_query($queryResultDelete);
+        removeProject($id_newProject);
+        
+        $this->assertTrue($check);
     }
 }
 
