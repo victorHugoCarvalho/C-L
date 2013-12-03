2de1b13b20c53864f95fab8c6f33a0b297b4457e
rodrigo14 <rodrigosg2000@hotmail.com>
2013-12-03 11:54:47 -0200

    Criação de teste para função check_project_manager

---
+++ cel/test/DataBaseCheckProjectManager.php
@@ -0,0 +1,39 @@
+<?php
+
+include ("../aplicacao/funcoes_genericas.php");
+include ("../aplicacao/dataBase/DatabaseProject.php");
+
+class DataBaseCheckProjectManager extends PHPUnit_Framework_TestCase
+{
+    public function testCheckProjectManagerReturnTrue()
+    {
+        $_SESSION['id_usuario_corrente'] = "Teste";
+        $id_newProject = includeProject("Nome Teste", "Descrição teste");
+        $id_user = 10;
+        $manager = 1;
+        
+        $query = "INSERT INTO participa (id_usuario, id_projeto, gerente) 
+                  VALUES ($id_user, $id_newProject, $manager)";
+        
+        $check = check_project_manager($id_user, $id_newProject);
+        $this->assertEquals(0, $check);
+        
+        removeProject($id_newProject);
+    }
+    
+    public function testCheckProjectManagerReturnFalse()
+    {
+        $_SESSION['id_usuario_corrente'] = "Teste";
+        $id_newProject = includeProject("Nome Teste", "Descrição teste");
+        $id_user = 10;
+        $manager = 0;
+        
+        $query = "INSERT INTO participa (id_usuario, id_projeto, gerente) 
+                  VALUES ($id_user, $id_newProject, $manager)";
+        
+        $check = check_project_manager($id_user, $id_newProject);
+        $this->assertEquals(0, $check);
+        
+        removeProject($id_newProject);
+    }
+}
