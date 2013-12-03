89e5e8fd6d2daccc34c1cc7930c7bbc8a2c9dd23
rodrigo14 <rodrigosg2000@hotmail.com>
2013-12-03 17:54:41 -0200

    retirar função removeCenario de funcoes_genericas.php

--- cel/aplicacao/funcoes_genericas.php
+++ cel/aplicacao/funcoes_genericas.php
@@ -3,6 +3,8 @@ include_once("bd.inc");
 include_once("bd_class.php");
 include_once("seguranca.php");
 
+include("dataBase/DatabaseRemoveCenario.php");
+
 
 /*
 if (!(class_exists("PGDB"))) {
@@ -519,40 +521,6 @@ if (!(function_exists("adicionar_lexico")))
             $nomesSinonimos[] = $rowSinonimo["nome"];
             $id_lexicoSinonimo[] = $rowSinonimo["id_lexico"];
         }
-    }
-}
-else
-{
-	//Nothing to do.
-}
-
-
-###################################################################
-# Essa funcao recebe um id de cenario e remove todos os seus
-# links e relacionamentos existentes.
-###################################################################
-if (!(function_exists("removeCenario")))
-{
-    function removeCenario($id_projeto, $id_cenario)
-    {
-    	assert($id_projeto != null, "id_projeto must not be null");
-    	assert($id_cenario != null, "id_cenario must not be null");
-    	
-        $DB = new PGDB () ;
-        $sql1 = new QUERY ($DB) ;
-        $sql2 = new QUERY ($DB) ;
-        $sql3 = new QUERY ($DB) ;
-        $sql4 = new QUERY ($DB) ;
-               
-        # Remove o relacionamento entre o cenario a ser removido
-        # e outros cenarios que o referenciam
-        $sql1->execute ("DELETE FROM centocen WHERE id_cenario_from = $id_cenario") ;
-        $sql2->execute ("DELETE FROM centocen WHERE id_cenario_to = $id_cenario") ;
-        # Remove o relacionamento entre o cenario a ser removido
-        # e o seu lexico
-        $sql3->execute ("DELETE FROM centolex WHERE id_cenario = $id_cenario") ;
-        # Remove o cenario escolhido
-        $sql4->execute ("DELETE FROM cenario WHERE id_cenario = $id_cenario") ;
     }
 }
 else
