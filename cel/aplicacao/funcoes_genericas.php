31d9844aaff9dcec2aad35fcf5867db97dac3a1b
rodrigo14 <rodrigosg2000@hotmail.com>
2013-12-03 17:33:35 -0200

    Retirar função check_project_manager de funcoes_genericas.php

--- cel/aplicacao/funcoes_genericas.php
+++ cel/aplicacao/funcoes_genericas.php
@@ -2119,33 +2119,4 @@ else
 	//Nothing to do.
 }
 
-
-###################################################################
-# Verifica se um determinado usuario e gerente de um determinado
-# projeto
-# Recebe o id do projeto. (1.1)
-# Faz um select para pegar o resultArray da tabela Participa.(1.2)
-# Se o resultArray for nao nulo: devolvemos TRUE(1);(1.3)
-# Se o resultArray for nulo: devolvemos False(0);(1.4)
-###################################################################
-function check_project_manager($id_usuario, $id_projeto)
-{
-	assert($id_usuario != null, "id_usuario must not be null");
-	assert($id_projeto != null, "id_projeto must not be null");
-	
-    $query = "SELECT * FROM participa WHERE gerente = 1 AND id_usuario = $id_usuario AND id_projeto = $id_projeto";
-    $queryResult = mysql_query($query) or die("Erro ao enviar a query de select no participa<br>" . mysql_error() . "<br>" . __FILE__ . __LINE__);
-    $resultArray = mysql_fetch_array($queryResult);
-    
-    if ($resultArray != false)
-    {
-        $manager = 1;
-    }
-    else
-    {
-    	$manager = 0;
-    }
-    
-    return $manager;
-}
 ?>
